7179a0cd67ebe21f62636d68df0cf181ed632c12
Ygor Ricardo Carneiro Fernandes de Araujo Gomes <ygor.dm@gmail.com>
2025-08-14 19:43:05 +0000

    Teste1

---
+++ testeCalduladora.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+
+    int num1, num2, num3;
+    printf("Digite o valor de num1: \n");
+    scanf("%i", &num1);
+    
+    printf("Digite o valro de num2: \n");
+    scanf("%i", &num2);
+
+    printf("Digite o valor de num3: \n");
+    scanf("%i", &num3);
+
+    printf("Valor de num1: \n %i \n num2: \n %i \n e num3: \n %i \n \n", num1, num2, num3);
+
+
+    return 0;
+
+}
